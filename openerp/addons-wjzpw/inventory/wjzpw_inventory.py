11f912a7f1bef1e06eb329ba0e824381f595a023
xxshutong <xxshutong@gmail.com>
2014-01-04 01:44:00 +0800

    Fix up reed inventory

--- openerp/addons-wjzpw/inventory/wjzpw_inventory.py
+++ openerp/addons-wjzpw/inventory/wjzpw_inventory.py
@@ -475,7 +475,7 @@ class wjzpw_reed_input(osv.osv):
 
     _columns = {
         'input_date': fields.date('wjzpw.inventory.ruKuRiQi', required=True),  # 入库日期
-        'reed_no': fields.float('wjzpw.inventory.kouHao', required=True),  # 扣号
+        'reed_no': fields.char('wjzpw.inventory.kouHao', required=True),  # 扣号
         'reed_width': fields.integer('wjzpw.inventory.kouFu', required=True),  # 扣辐
         'count': fields.integer('wjzpw.inventory.shuLiang', required=True),  # 数量
         'price': fields.float('wjzpw.inventory.danJia', required=True),  # 单价
@@ -723,7 +723,7 @@ class wjzpw_reed_inventory(osv.osv):
     _description = "wjzpw.inventory.kuCun"
 
     _columns = {
-        'reed_no': fields.integer('wjzpw.inventory.kouHao', readonly=True),  # 筘号
+        'reed_no': fields.char('wjzpw.inventory.kouHao', readonly=True),  # 筘号
         'reed_width': fields.integer('wjzpw.inventory.kouFu', readonly=True),  # 筘幅
         'count': fields.integer('wjzpw.inventory.shuLiang'),  # 数量
     }
@@ -740,10 +740,10 @@ class wjzpw_reed_inventory(osv.osv):
                 CASE
                     WHEN ((SELECT count(wro.id) AS count
                         FROM wjzpw_reed_output wro
-                        WHERE wro.reed_no = to_char(wri.reed_no, '999D99S') AND wro.reed_width = wri.reed_width)) <> 0
+                        WHERE wro.reed_no = wri.reed_no AND wro.reed_width = wri.reed_width)) <> 0
                         THEN
                             (sum(wri.count) -
-                            (SELECT sum(count) FROM wjzpw_reed_output wro WHERE wro.reed_no = to_char(wri.reed_no, '999D99S') AND wro.reed_width = wri.reed_width))
+                            (SELECT sum(count) FROM wjzpw_reed_output wro WHERE wro.reed_no = wri.reed_no AND wro.reed_width = wri.reed_width))
                         ELSE
                            sum(wri.count)
                 END AS count
